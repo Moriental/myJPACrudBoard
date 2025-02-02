94feb5d75b9449f5a5270c8d229b65d88707afaa
kim <youngwoo7804@gmail.com>
2025-02-02 18:50:53 +0900

    댓글 컨트롤러 구현

--- src/main/java/myCrudBoard/demo/controller/CommentController.java
+++ src/main/java/myCrudBoard/demo/controller/CommentController.java
@@ -8,7 +8,10 @@ import myCrudBoard.demo.domain.dto.CustomUserDetails;
 import myCrudBoard.demo.service.CommentService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/comments")
@@ -16,10 +19,23 @@ import org.springframework.web.bind.annotation.*;
 public class CommentController {
     private final CommentService commentService;
 
+    //댓글 생성 API
     @PostMapping
-    public ResponseEntity<CommentDTO> createComment(@RequestBody @Valid CommentDTO commentDTO, Authentication authentication) {
+    public ResponseEntity<?> createComment(@RequestBody @Valid CommentDTO commentDTO, BindingResult bindingResult, Authentication authentication) {
         CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
-        return null;
+
+        if(bindingResult.hasErrors()) {
+            return ResponseEntity.badRequest().body(bindingResult.getAllErrors());
+        }
+        CommentDTO savedComment = commentService.createComment(commentDTO, userDetails.getUsername());
+        return ResponseEntity.ok(savedComment);
+    }
+
+    //댓글 목록 조회 API
+    @GetMapping("/board/{boardId}")
+    public ResponseEntity<List<CommentDTO>> getCommentsByBoardId(@PathVariable Long boardId) {
+        List<CommentDTO> commentList = commentService.getComments(boardId);
+        return ResponseEntity.ok(commentList);
     }
 }
 
